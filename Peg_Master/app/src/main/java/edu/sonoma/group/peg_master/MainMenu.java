824a172c81504f003d3580f895663357bf60bc1b
student <none@none.com>
2016-02-28 14:23:00 -0800

    Added simple credits button and layout by Nick

--- Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/MainMenu.java
+++ Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/MainMenu.java
@@ -2,12 +2,24 @@ package edu.sonoma.group.peg_master;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+    private Button creditButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        //setContentView(R.layout.testlayout);
+        creditButton = (Button)findViewById(R.id.creditbutton);
+        creditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.credit_layout);
+            }
+        });
+
     }
 }
