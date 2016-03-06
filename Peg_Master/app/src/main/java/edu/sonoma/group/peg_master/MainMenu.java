7a9a7870daceabf45ce6e53f2e7f2d11c482e991
Lucas Winkemann <lwinkelmann94@gmail.com>
2016-03-06 13:45:57 -0800

    Added main menu concept with 3 buttons for info, options and starting the game. lw

--- Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/MainMenu.java
+++ Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/MainMenu.java
@@ -2,12 +2,36 @@ package edu.sonoma.group.peg_master;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Button startButton, infoButton, optionsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        startButton = (Button) findViewById(R.id.Startbutton);
+        infoButton = (Button) findViewById(R.id.Infobutton);
+        optionsButton = (Button) findViewById(R.id.Optionsbutton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.info_menu);
+            }
+        });
+        optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
