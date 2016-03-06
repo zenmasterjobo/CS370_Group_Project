9159cf0b6a7ff4d66856e395264706bfc105e7c7
student <none@none.com>
2016-03-06 14:37:12 -0800

    Added the Key Class implementation

--- Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/MainMenu.java
+++ Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/MainMenu.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
-    private Button creditButton;
 
     private Button startButton, infoButton, optionsButton;
 
