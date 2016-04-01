b7ba504b117e634d2560a152fc436b3c36a82c7a
avlisb <brandonsilva@outlook.com>
2016-03-31 17:56:32 -0700

    added check layout to board xml and temporary main menu image buttons

--- Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/BoardFragment.java
+++ Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/BoardFragment.java
@@ -1,6 +1,7 @@
 package edu.sonoma.group.peg_master;
 
 import android.app.Activity;
+import android.media.Image;
 import android.support.v4.app.FragmentTransaction;
 import android.content.Context;
 import android.net.Uri;
@@ -10,11 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by admin on 3/24/16.
  */
 public class BoardFragment extends Fragment {
+
+    private ImageButton chest1, chest2, chest3, chest4, chest5, chest6, chest7, chest8, chest9, chest10, chest11, chest12;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,8 @@ public class BoardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         //back = (Button) getView().findViewById(R.id.back);
         //back.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +43,10 @@ public class BoardFragment extends Fragment {
         //    }
         //});
         View rootView = inflater.inflate(R.layout.fragment_board, container, false);
+
+        chest1 = (ImageButton) rootView.findViewById(R.id.chest1);
+
+
         return rootView;
     }
 }
