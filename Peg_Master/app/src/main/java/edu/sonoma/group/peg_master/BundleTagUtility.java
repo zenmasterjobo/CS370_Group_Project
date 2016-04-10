25b5af1f5a5f4e21219b7672d0681482064a8dbe
Jordan Bergero <jordanbergero@Jordans-MacBook-Air.local>
2016-04-10 11:03:17 -0700

    Added a bundle tag utility, use this for handling save states -JB

---
+++ Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/BundleTagUtility.java
@@ -0,0 +1,9 @@
+package edu.sonoma.group.peg_master;
+
+/**
+ * Created by jordanbergero on 4/9/16.
+ */
+public class BundleTagUtility {
+    public static final String SWITCHSTATUS = "switch_status";
+    public static final String SWITCH1 =  "Switch1";
+}
