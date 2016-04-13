29c430ad37d295f175d1fe0f3b744d3c72ba0be5
nick <n.armour0@gmail.com>
2016-04-12 23:47:14 -0700

    database works, need to add more queries, only have add and getAllUsers

--- Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/MainMenu.java
+++ Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/MainMenu.java
@@ -44,6 +44,9 @@ public class MainMenu extends AppCompatActivity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
         mainMenuSound = MediaPlayer.create(this, R.raw.windwaker);
+
+
+
     }
 
     @Override
