337d81ccb9d0fe777897e6f8d03fc2e9499f1022
Lucas Winkemann <lwinkelmann94@gmail.com>
2016-04-13 10:38:40 -0700

    Added board.java basic ideas

--- Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/Board.java
+++ Peg_Master/app/src/main/java/edu/sonoma/group/peg_master/Board.java
@@ -4,8 +4,19 @@ package edu.sonoma.group.peg_master;
  * Created by admin on 4/9/16.
  */
 public class Board {
-    private 
-    public void generate(){
+    private int [][]board;
 
+    public void generate(int numChests){
+        int [] possible=new int[numChests];
+        for (int i=0;i <100;++i){
+            possible[i]=i;
+        }
+    }
+    public boolean isSolvable(int [][]board){
+        int count = 0;
+
+        if (count % 2 == 0)
+            return true;
+        return true;
     }
 }
